e6d4f796af2129db035e8e7c735eefebc1a8cb7d
T.J. Mazeika <mazeika.tj@gmail.com>
2017-03-18 23:13:50 -0400

    Extend master layout in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-    <meta name="theme-color" content="#ffffff">
-
-    <!-- favicon -->
-    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}" sizes="180x180">
-    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
-    <link rel="manifest" href="{{ asset('manifest.json') }}">
-    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
-
-    <title>PCForge</title>
-</head>
-<body>
-    <img src="{{ asset('/img/logo.svg') }}" width="200px"/>
-</body>
-</html>
+@section('content')
+<img src="{{ asset('/img/logo.svg') }}" width="200px"/>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}" sizes="180x180">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
+
+    <title>PCForge</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
