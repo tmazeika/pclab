580e6b05ea1b8ea7969a6a3643838ce07c6363d8
T.J. Mazeika <mazeika.tj@gmail.com>
2017-03-19 16:05:15 -0400

    Work on admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,17 +5,17 @@
 @section('content')
 <main class="content">
     @each('partials.admin-tables', [
-        \PCForge\ChassisComponent::all(),
-        \PCForge\ChassisFormFactor::all(),
-        \PCForge\Component::all(),
-        \PCForge\CoolingComponent::all(),
-        \PCForge\CoolingSocket::all(),
-        \PCForge\GraphicsComponent::all(),
-        \PCForge\MemoryComponent::all(),
-        \PCForge\MotherboardComponent::all(),
-        \PCForge\PowerComponent::all(),
-        \PCForge\ProcessorComponent::all(),
-        \PCForge\StorageComponent::all(),
-    ], 'items')
+        '\PCForge\ChassisComponent',
+        '\PCForge\ChassisFormFactor',
+        '\PCForge\Component',
+        '\PCForge\CoolingComponent',
+        '\PCForge\CoolingSocket',
+        '\PCForge\GraphicsComponent',
+        '\PCForge\MemoryComponent',
+        '\PCForge\MotherboardComponent',
+        '\PCForge\PowerComponent',
+        '\PCForge\ProcessorComponent',
+        '\PCForge\StorageComponent',
+    ], 'model')
 </main>
 @endsection
--- resources/views/partials/admin-tables.blade.php
+++ resources/views/partials/admin-tables.blade.php
@@ -1,50 +1,51 @@
-@if($items->count() > 0)
 <section>
     @php
-        $table = $items->first()->getTable();
+        $items = $model::all();
+        $table = (new $model)->getTable();
         $columns = Schema::getColumnListing($table);
     @endphp
 
     <h1>{{ $table }}</h1>
 
-    <div style="overflow-x: auto">
-        <table cellpadding="5">
-            <thead>
-                <tr style="font-weight: bold">
-                    @foreach($columns as $column)
-                        <td>{{ $column }}</td>
-                    @endforeach
-
-                    <td></td>
-                    <td></td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($items as $item)
-                    <tr>
+    @if($items->count() > 0)
+        <div style="overflow-x: auto">
+            <table cellpadding="5">
+                <thead>
+                    <tr style="font-weight: bold">
                         @foreach($columns as $column)
-                            <td>{{ $item->getAttribute($column) }}</td>
+                            <td>{{ $column }}</td>
                         @endforeach
 
-                        <td>
-                            <a href="{{ url('admin/update', [$table, $item->id]) }}">
-                                <button>Update</button>
-                            </a>
-                        </td>
-
-                        <td>
-                            <a href="{{ url('admin/delete', [$table, $item->id]) }}">
-                                <button>Delete</button>
-                            </a>
-                        </td>
+                        <td></td>
+                        <td></td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                    @foreach($items as $item)
+                        <tr>
+                            @foreach($columns as $column)
+                                <td>{{ $item->getAttribute($column) }}</td>
+                            @endforeach
+
+                            <td>
+                                <a href="{{ url('admin/update', [$table, $item->id]) }}">
+                                    <button>Update</button>
+                                </a>
+                            </td>
+
+                            <td>
+                                <a href="{{ url('admin/delete', [$table, $item->id]) }}">
+                                    <button>Delete</button>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endif
 
     <a href="{{ url('admin/create', [$table]) }}">
         <button>Create</button>
     </a>
 </section>
-@endif
