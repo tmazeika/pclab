0ffe4577186e21200ded4ed4c5c6d1565a062a96
T.J. Mazeika <mazeika.tj@gmail.com>
2017-03-20 23:15:47 -0400

    Style and work on logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,8 +9,10 @@
     <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
     <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
-    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#61d43a">
+    <meta name="apple-mobile-web-app-title" content="PCForge">
+    <meta name="application-name" content="PCForge">
+    <meta name="theme-color" content="#333333">
 
     <style>
         @font-face {
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,15 @@
 <header>
     <div class="content header-content">
         <a href="{{ url('/') }}">
-            <img class="header-logo" src="{{ asset('img/logo.svg') }}"/>
+            @include('svg.logo')
         </a>
 
         <div class="flex-spacer"></div>
 
         <a href="{{ url('build') }}">
-            <button class="header-button">Build</button>
+            <button class="header-button">
+                <img class="header-button-img" src="{{ url('img/wrench.svg') }}"/>
+            </button>
         </a>
     </div>
 </header>
