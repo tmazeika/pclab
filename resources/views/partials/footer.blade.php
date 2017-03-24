5328dd3eaf619d467cbb488aa9dd6378b4896053
T.J. Mazeika <mazeika.tj@gmail.com>
2017-03-23 21:43:43 -0400

    Add admin link in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,7 @@
 <footer>
     <div class="content footer-content">
         <span class="footer-item">&copy; {{ strftime('%Y') }} PCForge</span>
+        <!-- TODO -->
+        <span class="footer-item">&emsp;<a href="{{ url('admin') }}">Admin</a></span>
     </div>
 </footer>
