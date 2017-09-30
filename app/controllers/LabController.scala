c67a379555187cf93e1d98ac53f5d3dcef737bf1
T.J. Mazeika <mazeika.tj@gmail.com>
2017-09-30 11:31:26 -0400

    Pass in components

--- app/controllers/LabController.scala
+++ app/controllers/LabController.scala
@@ -3,10 +3,15 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
+import services.ComponentRepository
+
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class LabController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def index = Action {
-    Ok(views.html.lab())
+class LabController @Inject()(cc: ControllerComponents, cRepo: ComponentRepository)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+  def index = Action.async {
+    cRepo.all map { c ⇒
+      Ok(views.html lab c)
+    }
   }
 }
